8471f78ec0d0eeeb20b04d1465f12d3c074b5949
DmitriiPC <68027431+dumkaguv@users.noreply.github.com>
2025-05-31 13:37:45 +0300

    remove dist for Vercel deploy

---
+++ dist/assets/button-back-D3qqQvgL.js
@@ -0,0 +1,6 @@
+import{b as r,e as c,j as t,c as n}from"./index-BmMFpa1e.js";import{B as i}from"./product-card-image-BQJNy0bR.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=[["path",{d:"M18 15h-6v4l-7-7 7-7v4h6v6z",key:"lbrdak"}]],l=r("arrow-big-left",m),d=({children:a,className:e,...s})=>{const o=c();return t.jsx(i,{onClick:()=>o(-1),size:"lg",className:n("mb-10 gap-2 py-6 text-lg max-md:mb-6",e),...s,children:a||t.jsxs(t.Fragment,{children:[t.jsx(l,{size:40}),"Go Back"]})})};export{d as B};
